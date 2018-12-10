e3e0f85c49f11fa77640fd615ee1a72f5b2c1563
jm186140 <jennifer.medellin@teradata.com>
2018-12-10 13:55:49 -0600

    fix(icons): full raw paths for Skackblitz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,42 @@
-/*
- * Copyright (C) 2016-2017 by Teradata Corporation. All rights reserved.
- * TERADATA CORPORATION CONFIDENTIAL AND TRADE SECRET
- */
-
 import { Component } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-import { MatIconRegistry } from '@angular/material';
+import { MatIconRegistry } from '@angular/material/icon';
+import { TdMediaService, TdDigitsPipe } from '@covalent/core';
 
 @Component({
   selector: 'td-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
-  constructor(private _iconRegistry: MatIconRegistry,
-              private _domSanitizer: DomSanitizer) {
-    // Covalent Icons
-    this._iconRegistry.registerFontClassAlias('covalent', 'covalent-icons');
-
-    // SVG Icons
+  constructor(
+    private _iconRegistry: MatIconRegistry,
+    private _domSanitizer: DomSanitizer) {
     this._iconRegistry.addSvgIconInNamespace('assets', 'teradata',
-      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/teradata.svg'));
-      this._iconRegistry.addSvgIconInNamespace('assets', 'teradata-dark',
-        this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/teradata-dark.svg'));
-    this._iconRegistry.addSvgIconInNamespace('assets', 'github',
-      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/github.svg'));
+      this._domSanitizer.bypassSecurityTrustResourceUrl('https://raw.githubusercontent.com/Teradata/covalent-resources/feat/sandbox/src/assets/icons/teradata.svg'));
+    this._iconRegistry.addSvgIconInNamespace('assets', 'teradata-dark',
+      this._domSanitizer.bypassSecurityTrustResourceUrl('https://raw.githubusercontent.com/Teradata/covalent-resources/feat/sandbox/src/assets/icons/teradata-dark.svg'));
     this._iconRegistry.addSvgIconInNamespace('assets', 'covalent',
-      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/covalent.svg'));
+      this._domSanitizer.bypassSecurityTrustResourceUrl('https://raw.githubusercontent.com/Teradata/covalent-resources/feat/sandbox/src/assets/icons/covalent.svg'));
     this._iconRegistry.addSvgIconInNamespace('assets', 'covalent-mark',
-      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/covalent-mark.svg'));
-    this._iconRegistry.addSvgIconInNamespace('assets', 'teradata-ux',
-      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/teradata-ux.svg'));
-    this._iconRegistry.addSvgIconInNamespace('assets', 'appcenter',
-      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/appcenter.svg'));
-    this._iconRegistry.addSvgIconInNamespace('assets', 'listener',
-      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/listener.svg'));
-    this._iconRegistry.addSvgIconInNamespace('assets', 'querygrid',
-      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/querygrid.svg'));
-    
-    // SVG Icons
-    this._iconRegistry.addSvgIcon('teradata', this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/teradata.svg'));
-    this._iconRegistry.addSvgIcon('teradata-dark', this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/teradata-dark.svg'));
-    this._iconRegistry.addSvgIcon('teradata-solid', this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/teradata-solid.svg'));
-    this._iconRegistry.addSvgIcon('teradata-icon', this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/teradata-icon.svg'));
-    this._iconRegistry.addSvgIcon('github', this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/github.svg'));
-    this._iconRegistry.addSvgIcon('covalent', this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/covalent.svg'));
-    this._iconRegistry.addSvgIcon('covalent-mark', this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/covalent-mark.svg'));
-    this._iconRegistry.addSvgIcon('appcenter', this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/appcenter.svg'));
-    this._iconRegistry.addSvgIcon('control-center', this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/control-center.svg'));
-    this._iconRegistry.addSvgIcon('querygrid', this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/querygrid.svg'));
-    this._iconRegistry.addSvgIcon('listener', this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/listener.svg'));
-    this._iconRegistry.addSvgIcon('intellicloud', this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/intellicloud.svg'));
-    this._iconRegistry.addSvgIcon('workload-analytics', this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/workload-analytics.svg'));
-    this._iconRegistry.addSvgIcon('unity', this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/unity.svg'));
-    this._iconRegistry.addSvgIcon('viewpoint', this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/viewpoint.svg'));
+      this._domSanitizer.bypassSecurityTrustResourceUrl('https://raw.githubusercontent.com/Teradata/covalent-resources/feat/sandbox/src/assets/icons/covalent-mark.svg'));
+    this._iconRegistry.addSvgIconInNamespace('assets', 'github',
+      this._domSanitizer.bypassSecurityTrustResourceUrl('https://raw.githubusercontent.com/Teradata/covalent-resources/feat/sandbox/src/assets/icons/github.svg'));
+      this._iconRegistry.addSvgIconInNamespace('assets', 'app_center',
+      this._domSanitizer.bypassSecurityTrustResourceUrl('https://raw.githubusercontent.com/Teradata/covalent-resources/feat/sandbox/src/assets/icons/appcenter.svg'));
+      this._iconRegistry.addSvgIconInNamespace('assets', 'data-labs',
+      this._domSanitizer.bypassSecurityTrustResourceUrl('https://raw.githubusercontent.com/Teradata/covalent-resources/feat/sandbox/src/assets/icons/data-labs.svg'));
+      this._iconRegistry.addSvgIconInNamespace('assets', 'ingest',
+      this._domSanitizer.bypassSecurityTrustResourceUrl('https://raw.githubusercontent.com/Teradata/covalent-resources/feat/sandbox/src/assets/icons/ingest.svg'));
+      this._iconRegistry.addSvgIconInNamespace('assets', 'querygrid',
+      this._domSanitizer.bypassSecurityTrustResourceUrl('https://raw.githubusercontent.com/Teradata/covalent-resources/feat/sandbox/src/assets/icons/querygrid.svg'));
+      this._iconRegistry.addSvgIconInNamespace('assets', 'unity',
+      this._domSanitizer.bypassSecurityTrustResourceUrl('https://raw.githubusercontent.com/Teradata/covalent-resources/feat/sandbox/src/assets/icons/unity.svg'));
+      this._iconRegistry.addSvgIconInNamespace('assets', 'viewpoint',
+      this._domSanitizer.bypassSecurityTrustResourceUrl('https://raw.githubusercontent.com/Teradata/covalent-resources/feat/sandbox/src/assets/icons/viewpoint.svg'));
+      
+      this._iconRegistry.addSvgIconInNamespace('assets', 'workload-analytics',
+      this._domSanitizer.bypassSecurityTrustResourceUrl('https://raw.githubusercontent.com/Teradata/covalent-resources/feat/sandbox/src/assets/icons/workload-analytics.svg'));
   }
-
 }
