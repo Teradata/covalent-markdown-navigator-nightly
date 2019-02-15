df7242935f5a8e7f5ba779734386c07ef52408ba
jm186140 <jennifer.medellin@teradata.com>
2019-02-15 15:32:18 -0600

    chore(): upgrade version
    Upgrade from 2.0.0-beta-3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,7 @@ import { CovalentLoadingModule } from '@covalent/core/loading';
 import { CovalentMessageModule, CovalentStepsModule, CovalentDialogsModule } from '@covalent/core';
 import { CovalentHighlightModule } from '@covalent/highlight';
 import { CovalentBreadcrumbsModule } from '@covalent/core/breadcrumbs';
-import { CovalentHttpModule } from '@covalent/http';
+import { CovalentHttpModule } from '@covalent/http-deprec';
 
 import { appRoutes } from './app.routes';
 
