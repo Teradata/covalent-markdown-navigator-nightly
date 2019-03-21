b8479d05c064dce3557ef9db2d451bdc24ffe22a
joejcarey <joe.carey@teradata.com>
2019-03-21 14:40:44 -0500

    moved pattern breadcrumbs into own pr for review

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,20 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { MainComponent } from './main.component';
 import { OverviewComponent } from './overview/overview.component';
+import { BreadcrumbsComponent } from './patterns/breadcrumbs/breadcrumbs.component';
 
-const routes: Routes = [{
-  path: '',
-  component: MainComponent,
-  children: [
-    { path: '', component: OverviewComponent },
-  ],
-},
+const routes: Routes = [
+  { path: 'layouts/nav-view', redirectTo: '/', pathMatch: 'full' },
+  {
+    path: '',
+    component: MainComponent,
+    children: [{ path: '', component: OverviewComponent }],
+  },
+  {
+    path: 'patterns',
+    component: MainComponent,
+    children: [{ path: 'breadcrumbs', component: BreadcrumbsComponent }],
+  },
 ];
 
 export const appRoutes: any = RouterModule.forRoot(routes);
---
+++ src/app/patterns/breadcrumbs/breadcrumbs.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+import { TdLoadingService } from '@covalent/core/loading';
+import { TdMediaService } from '@covalent/core/media';
+
+import { CovalentBreadcrumbsModule } from '@covalent/core/breadcrumbs';
+import { baseURL } from '../../../data';
+
+@Component({
+  selector: 'qs-breadcrumbs',
+  templateUrl: './breadcrumbs.component.html',
+  styleUrls: ['./breadcrumbs.component.scss'],
+})
+export class BreadcrumbsComponent implements OnInit {
+
+  constructor(private _titleService: Title,
+              private _loadingService: TdLoadingService,
+              private _changeDetectorRef: ChangeDetectorRef,
+              public media: TdMediaService) {
+    Object.assign(this, { baseURL });
+  }
+
+  ngOnInit(): void {
+    this._titleService.setTitle('Breadcrumbs');
+  }
+}
