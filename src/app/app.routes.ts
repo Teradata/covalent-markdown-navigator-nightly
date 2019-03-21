2570582dde9a7b5f340e2dd369807096a340afef
joejcarey <joe.carey@teradata.com>
2019-03-21 11:02:50 -0500

    moved pattern nav drawer into own pr for review

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,20 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { MainComponent } from './main.component';
 import { OverviewComponent } from './overview/overview.component';
+import { NavDrawerComponent } from './patterns/nav-drawer/nav-drawer.component';
 
-const routes: Routes = [{
-  path: '',
-  component: MainComponent,
-  children: [
-    { path: '', component: OverviewComponent },
-  ],
-},
+const routes: Routes = [
+  { path: 'layouts/nav-view', redirectTo: '/', pathMatch: 'full' },
+  {
+    path: '',
+    component: MainComponent,
+    children: [{ path: '', component: OverviewComponent }],
+  },
+  {
+    path: 'patterns',
+    component: MainComponent,
+    children: [{ path: 'nav-drawer', component: NavDrawerComponent }],
+  },
 ];
 
 export const appRoutes: any = RouterModule.forRoot(routes);
