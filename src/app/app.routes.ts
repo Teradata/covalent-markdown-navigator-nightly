e67c57dcc4175ae53a928cc8df029679b978017e
joejcarey <joe.carey@teradata.com>
2019-03-21 15:27:52 -0500

    moving layouts nav view into own pr for review

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,20 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { MainComponent } from './main.component';
 import { OverviewComponent } from './overview/overview.component';
+import { NavViewComponent } from './layouts/nav-view/nav-view.component';
 
-const routes: Routes = [{
-  path: '',
-  component: MainComponent,
-  children: [
-    { path: '', component: OverviewComponent },
-  ],
-},
+const routes: Routes = [
+  { path: 'patterns/creation-flow', redirectTo: '/', pathMatch: 'full' },
+  {
+    path: '',
+    component: MainComponent,
+    children: [{ path: '', component: OverviewComponent }],
+  },
+  {
+    path: 'layouts',
+    component: MainComponent,
+    children: [{ path: 'nav-view', component: NavViewComponent }],
+  },
 ];
 
 export const appRoutes: any = RouterModule.forRoot(routes);
---
+++ src/app/layouts/nav-view/nav-view.component.ts
@@ -0,0 +1,115 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { baseURL } from '../../../data';
+
+@Component({
+  selector: 'qs-nav-view',
+  templateUrl: './nav-view.component.html',
+  styleUrls: ['./nav-view.component.scss'],
+})
+export class NavViewComponent implements OnInit {
+
+  productList: Object[] = [
+    {
+      title: 'AppCenter',
+      icon: 'app_center',
+      health: 'RUNNING',
+      status: 'teal-700',
+      color: 'primary',
+    },
+    {
+      title: 'Data Labs',
+      icon: 'data-labs',
+      health: 'AVAILABLE',
+      status: 'orange-700',
+      color: 'accent',
+    },
+    {
+      title: 'Listener',
+      icon: 'ingest',
+      health: 'AVAILABLE',
+      status: 'orange-700',
+      color: 'accent',
+    },
+    {
+      title: 'QueryGrid',
+      icon: 'querygrid',
+      health: 'AVAILABLE',
+      status: 'orange-700',
+      color: 'primary',
+    },
+    {
+      title: 'Unity',
+      icon: 'unity',
+      health: 'AVAILABLE',
+      status: 'orange-700',
+      color: 'warn',
+    },
+    {
+      title: 'Viewpoint',
+      icon: 'viewpoint',
+      health: 'RUNNING',
+      status: 'teal-700',
+      color: 'primary',
+    },
+    {
+      title: 'Workload Analytics',
+      icon: 'workload-analytics',
+      health: 'RUNNING',
+      status: 'teal-700',
+      color: 'primary',
+    },
+  ];
+
+  serviceList: Object[] = [
+    {
+      title: 'Audit Service',
+      icon: 'verified_user',
+      health: 'HEALTHY',
+      status: 'teal-700',
+    },
+    {
+      title: 'Authentication Service',
+      icon: 'phonelink_lock',
+      health: 'HEALTHY',
+      status: 'teal-700',
+    },
+    {
+      title: 'Dictionary Service',
+      icon: 'data_usage',
+      health: 'AVAILABLE',
+      status: 'orange-700',
+    },
+    {
+      title: 'Notifications Service',
+      icon: 'email',
+      health: 'HEALTHY',
+      status: 'teal-700',
+    },
+    {
+      title: 'Query Service',
+      icon: 'desktop_windows',
+      health: 'HEALTHY',
+      status: 'teal-700',
+    },
+    {
+      title: 'System Management',
+      icon: 'dns',
+      health: 'HEALTHY',
+      status: 'teal-700',
+    },
+    {
+      title: 'User Management',
+      icon: 'people',
+      health: 'HEALTHY',
+      status: 'teal-700',
+    },
+  ];
+
+  constructor(private _titleService: Title) {}
+
+  ngOnInit(): void {
+    this._titleService.setTitle( 'Teradata Covalent - Sandbox' );
+    Object.assign(this, { baseURL });
+  }
+}
