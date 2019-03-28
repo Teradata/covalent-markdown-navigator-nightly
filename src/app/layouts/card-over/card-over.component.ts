f701b170af3c702c1dfcf201c92341b443f317be
joejcarey <joe.carey@teradata.com>
2019-03-28 14:30:51 -0500

    fixed linting errors

--- src/app/layouts/card-over/card-over.component.ts
+++ src/app/layouts/card-over/card-over.component.ts
@@ -9,78 +9,89 @@ import { baseURL } from '../../../data';
   styleUrls: ['./card-over.component.scss'],
 })
 export class CardOverComponent implements OnInit {
-
-  constructor(private _titleService: Title,
-    public media: TdMediaService) {
-      Object.assign(this, { baseURL });
-  }
-  
-  routes: Object[] = [{
+  routes: Object[] = [
+    {
       icon: 'home',
       route: '.',
       title: 'Home',
-  }, {
-    icon: 'library_books',
-    route: '.',
-    title: 'Documentation',
-  }, {
-    icon: 'color_lens',
-    route: '.',
-    title: 'Style Guide',
-  }, {
-    icon: 'view_quilt',
-    route: '.',
-    title: 'Layouts',
-  }, {
-    icon: 'picture_in_picture',
-    route: '.',
-    title: 'Components & Addons',
-  },
+    },
+    {
+      icon: 'library_books',
+      route: '.',
+      title: 'Documentation',
+    },
+    {
+      icon: 'color_lens',
+      route: '.',
+      title: 'Style Guide',
+    },
+    {
+      icon: 'view_quilt',
+      route: '.',
+      title: 'Layouts',
+    },
+    {
+      icon: 'picture_in_picture',
+      route: '.',
+      title: 'Components & Addons',
+    },
   ];
-  usermenu: Object[] = [{
-    icon: 'swap_horiz',
-    route: '.',
-    title: 'Switch account',
-  }, {
-    icon: 'tune',
-    route: '.',
-    title: 'Account settings',
-  }, {
-    icon: 'exit_to_app',
-    route: '.',
-    title: 'Sign out',
-  },
+  usermenu: Object[] = [
+    {
+      icon: 'swap_horiz',
+      route: '.',
+      title: 'Switch account',
+    },
+    {
+      icon: 'tune',
+      route: '.',
+      title: 'Account settings',
+    },
+    {
+      icon: 'exit_to_app',
+      route: '.',
+      title: 'Sign out',
+    },
   ];
-  cardlist: Object[] = [{
-    icon: 'account_box',
-    route: '.',
-    title: 'John Jameson',
-    description: 'Owner',
-  }, {
-    icon: 'description',
-    route: '.',
-    title: 'An item description',
-    description: 'Description',
-  }, {
-    icon: 'vpn_key',
-    route: '.',
-    title: '1141e8e8-8d24-4956-93c2',
-    description: 'API Key',
-  },
+  cardlist: Object[] = [
+    {
+      icon: 'account_box',
+      route: '.',
+      title: 'John Jameson',
+      description: 'Owner',
+    },
+    {
+      icon: 'description',
+      route: '.',
+      title: 'An item description',
+      description: 'Description',
+    },
+    {
+      icon: 'vpn_key',
+      route: '.',
+      title: '1141e8e8-8d24-4956-93c2',
+      description: 'API Key',
+    },
   ];
-  carddates: Object[] = [{
+  carddates: Object[] = [
+    {
       icon: 'access_time',
       route: '.',
       date: '2017-07-07T00:25:49+00:00',
       description: 'Last Updated',
-    }, {
+    },
+    {
       icon: 'today',
       route: '.',
       date: '2017-07-04T00:25:49+00:00',
       description: 'Created',
     },
   ];
-  
+
+  constructor(private _titleService: Title, public media: TdMediaService) {
+    Object.assign(this, { baseURL });
+  }
+
   ngOnInit(): void {
     this._titleService.setTitle('Card Over');
   }
