2f7603b85bab043941ed91b0e0088fb1d24438b6
joejcarey <34658520+joejcarey@users.noreply.github.com>
2019-06-17 16:12:49 -0500

    feat(): layout card over (#29)
    * moving layouts card over into own pr for review
    
    * fixed linting errors
    
    * removed the unused imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,20 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { MainComponent } from './main.component';
 import { OverviewComponent } from './overview/overview.component';
+import { CardOverComponent } from './layouts/card-over/card-over.component';
 
-const routes: Routes = [{
-  path: '',
-  component: MainComponent,
-  children: [
-    { path: '', component: OverviewComponent },
-  ],
-},
+const routes: Routes = [
+  { path: 'patterns/creation-flow', redirectTo: '/', pathMatch: 'full' },
+  {
+    path: '',
+    component: MainComponent,
+    children: [{ path: '', component: OverviewComponent }],
+  },
+  {
+    path: 'layouts',
+    component: MainComponent,
+    children: [{ path: 'card-over', component: CardOverComponent }],
+  },
 ];
 
 export const appRoutes: any = RouterModule.forRoot(routes);
